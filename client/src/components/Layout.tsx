56d755bc28da3416def0b1d4d69c80105c29bf7c
hustlerZzZ <rishabpal2000@gmail.com>
2024-12-30 21:33:43 +0530

    Added Tailwind Css

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,21 +1,9 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "./essentials/Navbar.tsx";
-import Footer from "./essentials/Footer.tsx";
-import styled from "styled-components";
-
-const Wrapper = styled.div`
-  display: grid;
-  height: 100vh;
-  grid-template-rows: auto 1fr auto;
- 
-`;
 
 export default function Layout() {
   return (
-    <Wrapper>
-      <Navbar />
+    <div>
       <Outlet />
-      <Footer />
-    </Wrapper>
+    </div>
   );
 }
